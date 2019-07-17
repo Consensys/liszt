865ee88c4472b3902683f7ac5bea55c5fabd8ea6
Nicolas Liochon <liochon@gmail.com>
2019-07-17 11:44:03 +0200

    add services for x-transfers

--- blockchainmanager/src/main/java/net/consensys/liszt/blockchainmanager/Services.java
+++ blockchainmanager/src/main/java/net/consensys/liszt/blockchainmanager/Services.java
@@ -1,5 +1,7 @@
 package net.consensys.liszt.blockchainmanager;
 
+import java.util.List;
+
 public interface Services {
 
   /**
@@ -15,4 +17,19 @@ public interface Services {
    * @return the hash of the block where the transaction was included, if any
    */
   byte[] checkInclusion(byte[] rootHash);
+
+
+  /**
+   * Get the list of cross transfers with this rollup id as the destination that are in locked done but not in
+   *  transfer done state. This is called by the operator when he starts a batch, so see if there
+   *  are transfers that can done in the current time limit.
+   */
+  List<LockDone> getLockDone(int rollupId);
+
+
+  /**
+   * Check if there is a lock and the transfer is not done for this transfer.
+   * @return the timeout (a block height) if the locked in done, 0 otherwise
+   */
+  int getLockedDone(int rollupId, byte[] txHash);
 }
