7e120b130d2078f8eb1f49c2da9c0306d3751617
bkolad <blazejkolad@gmail.com>
2019-09-12 23:07:05 +0200

    table fix

--- liszt-ui/src/components/AccountTable.vue
+++ liszt-ui/src/components/AccountTable.vue
@@ -2,26 +2,25 @@
   <div>
     <b-table :items="accounts" :fields="fields" striped sticky-header ="300px" bordered borderless head-variant	="dark" responsive="sm">
 
-      <template slot="[index]" slot-scope="data">
+     <template v-slot:cell(index)="data">
         {{ data.index + 1 }}
       </template>
 
-       <template slot="[owner]" slot-scope="data">
+      <template v-slot:cell(owner)="data">
          <b-row class="mb-2">
-
           <b-col> {{data.item.publicKey | truncate(15)}} </b-col>
           <b-col> <b-button size="sm" @click="$emit('load:data', data.index)" class="mr-2"> copy address </b-button> </b-col>
         </b-row>
 
        </template>
 
-      <template slot="[show_details]" slot-scope="data">
+      <template v-slot:cell(show_details)="data">
         <b-button size="sm" @click="data.toggleDetails" class="mr-2">
           {{ data.detailsShowing ? 'Hide' : 'Show'}} Details
         </b-button>
       </template>
 
-      <template slot="row-details" slot-scope="data">
+      <template v-slot:cell(row-details)="data">
         <b-card>
           <b-row class="mb-2">
             <b-col sm="3" class="text-sm-right"><b>Nonce:</b></b-col>
